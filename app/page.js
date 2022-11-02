fd35c0a98ffaa1fe857b62c17ef1fdbfeb4bd33a
baronrocketmail <baronrocketmail@yahoo.com>
2022-11-01 22:41:57 -0700

    d

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 
+async function getName(){
+    let info = await fetch("http://localhost:3000/api/name")
+    return info.json()
+}
 
-export default function Page(){
+export default async function Page(){
+
+    const name = await getName()
+
     return(
-            <h1>23</h1>
+            <h1>{name}</h1>
     )
 }
